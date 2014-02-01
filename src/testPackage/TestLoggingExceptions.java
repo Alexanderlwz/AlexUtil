93e5aff3b5bf987f2f010828f7610a57b49fe626
Alexanderlwz <alexander.lwz@gmail.com>
2014-02-01 22:20:00 +0800

    TestLoggingExceptions

---
+++ src/testPackage/TestLoggingExceptions.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+import local.util.LoggingException;
+
+public class TestLoggingExceptions {
+	public static void main(String[] args) {
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e) {
+			System.err.println("Caught " + e);
+		}
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e) {
+			System.out.println("Caught " + e);
+		}
+	}
+}

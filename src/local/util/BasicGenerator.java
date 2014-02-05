297dc36107f659e09469b4b18ab0b30205d8fc45
Alexanderlwz <alexander.lwz@gmail.com>
2014-02-05 15:04:48 +0800

    Default Generator

---
+++ src/local/util/BasicGenerator.java
@@ -0,0 +1,40 @@
+/**
+ * CopyRight (C) 1989-2989 <Alexander>
+ * Copy Right Author     : Alexander_LWZ
+ * Package Space         : local.util
+ * JDK Version Used      : jdk1.7.0_45
+ * Comments              : Automatically create a Generator by given a class
+ * Version               : 1.0.0
+ * Create Date           : 2014.2.5
+ * Modification History  
+ * Sr Date       Why & What is modified
+ */
+
+package local.util;
+
+public class BasicGenerator<T> implements Generator<T> {
+	private Class<T> type;
+
+	public BasicGenerator(Class<T> type) {
+		this.type = type;
+	}
+
+	public T next() {
+		try {
+			return type.newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+	}
+
+	/**
+	 * Produce a Default generator given a type token
+	 * 
+	 * @param type
+	 *            type token
+	 * @return generator
+	 */
+	public static <T> Generator<T> create(Class<T> type) {
+		return new BasicGenerator<T>(type);
+	}
+}
---
+++ src/local/util/Generator.java
@@ -0,0 +1,17 @@
+/**
+ * CopyRight (C) 1989-2989 <Alexander>
+ * Copy Right Author     : Alexander_LWZ
+ * Package Space         : local.util
+ * JDK Version Used      : jdk1.7.0_45
+ * Comments              : The interface for all Generator
+ * Version               : 1.0.0
+ * Create Date           : 2014.2.5
+ * Modification History  
+ * Sr Date       Why & What is modified
+ */
+
+package local.util;
+
+public interface Generator<T> {
+	T next();
+}
